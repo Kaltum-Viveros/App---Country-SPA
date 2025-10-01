d96f3c73a3712748686d8f059d81a21c91d55b5b
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-09-30 21:15:20 -0600

    rutas country con el layout

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,22 +1,24 @@
 import { Routes } from "@angular/router";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { CountryLayoutComponent } from "./layouts/country-layout/country-layout.component";
 
 export const countryRoutes: Routes = [
-
     {
         path: '',
-        component: ByCapitalPageComponent,
+        component: CountryLayoutComponent,
+        children: [
+            {
+                path: 'by-capital',
+                component: ByCapitalPageComponent,
+            },
+
+            {
+                path: '**',
+                redirectTo: 'by-capital',
+            },
+        ],
     },
-
-    /*{
-        path: 'country',
-        //
-    },*/
-
-    /*{
-        path: '**',
-        redirectTo: '',
-    },*/
+    
 ];
 
 export default countryRoutes;
