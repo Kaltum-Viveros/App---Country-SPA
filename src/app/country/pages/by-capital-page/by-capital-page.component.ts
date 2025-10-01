d4a0ad7af520237148dc02f64e10888606e5f254
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-10-01 14:30:02 -0600

    busqueda por pais

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,20 +12,40 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({request}) => {
+      if(!request.query) return [];
+
+      return await firstValueFrom( //firstValueFrom convierte un observable en una promesa que es lo que espera el resource
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
+
+  /*isLoading = signal(false);
   isError = signal<string | null>(null);
-  countries = signal<Country[]>([]); 
+  countries = signal<Country[]>([]);
 
   onSearch(query: string) {
     if (this.isLoading()) return;
-    
+
     this.isLoading.set(true);
     this.isError.set(null);
 
-    this.countryService.searchByCapital(query).subscribe((countries) => {
-      this.isLoading.set(false);
-      this.countries.set(countries);
+    this.countryService.searchByCapital(query)
+    .subscribe({
+      next: (countries) => {
+          this.isLoading.set(false);
+          this.countries.set(countries);
+      },
+      error : (err) => {
+        this.isLoading.set(false);
+        this.countries.set([]);
+        this.isError.set(err);
+      }
     });
-  }
+  }*/
 }
