2ccf15fe67a29f68342c96efd5f540eeea156222
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-10-01 12:59:55 -0600

    mapper Country

---
+++ src/app/country/mappers/country.mapper.ts
@@ -0,0 +1,23 @@
+import type { Country } from '../interfaces/country.interface';
+import type { RESTCountry } from '../interfaces/rest-countries.interfaces';
+
+export class CountryMapper {
+  // esta es una función que recibe un RESTCountry y devuelve un Country para mapear los datos de la API a la interfaz que usamos en la app
+    static mapRestCountryToCountry(restCountry: RESTCountry): Country {
+      return {
+          capital: restCountry.capital.join(', '),
+          cca2: restCountry.cca2,
+          flag: restCountry.flag,
+          flagSvg: restCountry.flags.svg,
+          name: restCountry.name.common,
+          population: restCountry.population,
+      };
+    }
+
+  // esta es una función que recibe un array de RESTCountry y devuelve un array de Country para mapear los datos de la API a la interfaz que usamos en la app
+    static mapRestCountryArrayToCountryArray(
+        restCountries: RESTCountry[]
+    ): Country[] {
+        return restCountries.map(this.mapRestCountryToCountry);
+    }
+}
