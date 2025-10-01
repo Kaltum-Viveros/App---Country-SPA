5acc3aac63412cc868e2a62c6da2baebede37a2f
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-10-01 02:43:27 -0600

    avance tabla by capital

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { SearchInputComponent } from "../../components/search-input/search-input.component";
-import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { Component, inject, signal } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
+import { RESTCountry } from '../../interfaces/rest-countries.interfaces';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,9 +10,22 @@ import { CountryListComponent } from "../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
 
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<RESTCountry[]>([]);
 
-  onSearch(value:string) {
-    console.log({value});
+  onSearch(query: string) {
+    if (this.isLoading()) return;
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(query).subscribe((countries) => {
+      this.isLoading.set(false);
+      this.countries.set(countries);
+
+      console.log({ countries });
+    });
   }
 }
