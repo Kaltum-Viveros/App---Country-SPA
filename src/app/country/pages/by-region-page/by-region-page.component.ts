cb950c92bb9128a16acc90be58eaea61fcc6858a
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-10-01 23:37:14 -0600

    Pagina terminada, sin despliegue

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -2,19 +2,32 @@ import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 
-import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { Region } from '../../interfaces/region.type';
 import { CountryService } from '../../services/country.service';
 import { ActivatedRoute, Router } from '@angular/router';
+
+function validateQueryParam(queryParam: string): Region {
+  queryParam = queryParam.toLowerCase();
+  const validRegions: Record<string, Region> = {
+    africa: 'Africa',
+    americas: 'Americas',
+    asia: 'Asia',
+    europe: 'Europe',
+    oceania: 'Oceania',
+    antarctic: 'Antarctic',
+  };
+
+  return validRegions[queryParam] ?? 'Americas';
+}
 
 @Component({
   selector: 'app-by-region-page',
   imports: [CountryListComponent],
   templateUrl: './by-region-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ByRegionPageComponent {
-  
   countryService = inject(CountryService);
 
   public regions: Region[] = [
@@ -23,24 +36,25 @@ export class ByRegionPageComponent {
     'Asia',
     'Europe',
     'Oceania',
-    'Antarctic'
+    'Antarctic',
   ];
 
   activatedRoute = inject(ActivatedRoute);
-  router = inject(Router)
+  router = inject(Router);
 
-  queryParam = (this.activatedRoute.snapshot.queryParamMap.get('region') ?? '') as Region;
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('region') ?? '';
 
-  selectedRegion = linkedSignal<Region>(() => this.queryParam ?? 'Americas');
+  selectedRegion = linkedSignal<Region>(() =>
+    validateQueryParam(this.queryParam)
+  );
 
   countryResource = rxResource({
     request: () => ({ region: this.selectedRegion() }),
     loader: ({ request }) => {
       if (!request.region) return of([]); //of es de rxjs y crea un observable a partir de un valor, es una array vacio porque no hay query
 
-      this.router.navigate(['/country/by-region'],{
-        queryParams: 
-        {
+      this.router.navigate(['/country/by-region'], {
+        queryParams: {
           region: request.region,
         },
       });
@@ -48,5 +62,4 @@ export class ByRegionPageComponent {
       return this.countryService.searchByRegion(request.region);
     },
   });
-
 }
