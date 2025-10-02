91a097dfaeb10eed3795243559c7b7ae471a3ab1
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-10-01 20:38:12 -0600

    input con debounce

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,14 +1,27 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
   imports: [],
   templateUrl: './search-input.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SearchInputComponent {
-
   placeholder = input('Buscar');
   value = output<string>();
+  debounceTime = input(300);
 
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  });
 }
